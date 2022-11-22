b2341d6c32a38e6ca9ee9556d0339d680f94a728
Rizky Prayatman <r.prayatman@gmail.com>
2022-11-22 22:03:46 +0800

    add feature get all product

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -3,6 +3,19 @@ const { Product } = require("../db/models");
 // CRUD
 
 // create a method/function to get all products
+exports.getAllProduct = async (request, response) => {
+  try {
+    const product = await Product.findAll();
+    if (!product) {
+      response.status(404).json({ message: "Not found" });
+      return;
+    }
+    response.status(200).json({product});
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ message: "Internal server error" });
+  }
+};
 
 // create a method/function to get a single product
 
@@ -30,19 +43,32 @@ exports.updateProduct = async (request, response) => {
   }
 };
 
-exports.createProduct = async (req, res ) => {
+exports.createProduct = async (req, res) => {
   try {
     const { name, description, price } = req.body;
-    if(!name || !description || !price){
-      res.status(404).json({message: "Bad Request"});
+    if (!name || !description || !price) {
+      res.status(404).json({ message: "Bad Request" });
       return;
     }
     const result = await Product.create(req.body);
-    res.status(200).json({message: "OK", Data:result});
+    res.status(200).json({ message: "OK", Data: result });
   } catch (error) {
-    res.status(500).json({messag: "Internal Server Error"});
+    res.status(500).json({ messag: "Internal Server Error" });
   }
 };
 
-
 // create a method/function to delete a product
+exports.deleteProduct = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await Product.delete(id, res);
+    if (!data) {
+      response.status(404).json({ message: "Not found" });
+      return;
+    }
+    data.destroy();
+    res.status(200).json({ message: "Delete"});
+  } catch (error) {
+    res.status(500).json({ messag: "Internal Server Error" });
+  }
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const ProductController = require("../controllers/product.controller");
 const router = Router();
 
 // create a route to get all products with GET method and /api/products path and ProductController.getAllProducts as a callback
-
+router.get("/api/products", ProductController.getAllProduct);
 // create a route to get a single product with GET method and /api/products/:id path and ProductController.getProduct as a callback
 
 // create a route to create a product with POST method and /api/products path and ProductController.createProduct as a callback
@@ -13,5 +13,6 @@ router.post("/api/products", ProductController.createProduct);
 router.put("/api/products/:id", ProductController.updateProduct);
 
 // create a route to delete a product with DELETE method and /api/products/:id path and ProductController.deleteProduct as a callback
+router.delete("/api/products/:id", ProductController.deleteProduct);
 
 module.exports = router;
