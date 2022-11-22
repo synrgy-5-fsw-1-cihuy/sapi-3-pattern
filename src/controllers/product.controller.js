3313abe01646a4a9cdaf08f7edc756858f7be450
Khoironi Kurnia Syah <zekhoi.app@gmail.com>
2022-11-22 18:52:29 +0800

    if u wanna see

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -3,10 +3,50 @@ const { Product } = require("../db/models");
 // CRUD
 
 // create a method/function to get all products
+// READ all product
+exports.getAllProducts = async (request, response) => {
+  try {
+    const result = await Product.findAll();
+    response.json({ message: "OK", data: result });
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ message: "Internal server error" });
+  }
+};
 
 // create a method/function to get a single product
+// READ a product
+exports.getProduct = async (request, response) => {
+  try {
+    const result = await Product.findByPk(request.params.id);
+
+    if (!result) {
+      res.status(404).json({ message: "Not Found" });
+      return;
+    }
+
+    response.json({ message: "OK", data: result });
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ message: "Internal server error" });
+  }
+};
 
 // create a method/function to create a product
+// CREATE a product
+exports.createProduct = async (req, res) => {
+  try {
+    const { name, description, price } = req.body;
+    if (!name || !description || !price) {
+      res.status(404).json({ message: "Bad Request" });
+      return;
+    }
+    const result = await Product.create(req.body);
+    res.status(200).json({ message: "OK", Data: result });
+  } catch (error) {
+    res.status(500).json({ messag: "Internal Server Error" });
+  }
+};
 
 // create a method/function to update a product
 // UPDATE a product
@@ -30,19 +70,21 @@ exports.updateProduct = async (request, response) => {
   }
 };
 
-exports.createProduct = async (req, res ) => {
+// create a method/function to delete a product
+// DELETE a product
+exports.deleteProduct = async (request, response) => {
   try {
-    const { name, description, price } = req.body;
-    if(!name || !description || !price){
-      res.status(404).json({message: "Bad Request"});
+    const result = await Car.destroy({ where: { id: request.params.id } });
+    if (!result) {
+      res.status(404).json({ message: "Car not found" });
       return;
     }
-    const result = await Product.create(req.body);
-    res.status(200).json({message: "OK", Data:result});
+
+    res.json({
+      message: "OK",
+    });
   } catch (error) {
-    res.status(500).json({messag: "Internal Server Error"});
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
-
-
-// create a method/function to delete a product
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,14 +4,18 @@ const ProductController = require("../controllers/product.controller");
 const router = Router();
 
 // create a route to get all products with GET method and /api/products path and ProductController.getAllProducts as a callback
+router.get("/api/products", ProductController.getAllProducts);
 
 // create a route to get a single product with GET method and /api/products/:id path and ProductController.getProduct as a callback
+router.get("/api/products/:id", ProductController.getProduct);
 
 // create a route to create a product with POST method and /api/products path and ProductController.createProduct as a callback
 router.post("/api/products", ProductController.createProduct);
+
 // create a route to update a product with PUT method and /api/products/:id path and ProductController.updateProduct as a callback
 router.put("/api/products/:id", ProductController.updateProduct);
 
 // create a route to delete a product with DELETE method and /api/products/:id path and ProductController.deleteProduct as a callback
+router.delete("/api/products/:id", ProductController.deleteProduct);
 
 module.exports = router;
